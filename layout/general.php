62a31c0c64e39fd692c1b92c043cb841f66fe0fa
Mikhail Baranov <mikhail.baranov@gmail.com>
2011-11-21 11:47:23 +0400

    We are finished base layout styling.

--- layout/general.php
+++ layout/general.php
@@ -44,124 +44,124 @@ echo $OUTPUT->doctype() ?>
 <body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)) ?>">
     <?php echo $OUTPUT->standard_top_of_body_html() ?>
     <div id="page">
-        <?php if ($hasheading || $hasnavbar) { ?>
+        <?php if ($hasheading) { ?>
         <div id="header">
+            <div id="logo">
+                <?php if ($haslogo) {
+                        echo html_writer::link(new moodle_url('/'), "<img src='".$PAGE->theme->settings->logo."' alt='logo' class='logo' />");
+                    } ?>
+                <h5 class="subtitle"><?php echo $tagline ?></h5>
+
+                <h2 class="site-name"><a href="/"><?php echo $PAGE->heading ?></a></h2>
+            </div>
+            <div id="userbar">
+                    <?php if (isloggedin()) {
+                        echo html_writer::start_tag('ul', array('class'=>"userbsar-links"));
+                        echo html_writer::tag('span', $OUTPUT->user_picture($USER, array('size'=>24)), array('class'=>'userimg'));
+                        echo html_writer::start_tag('li', array('id'=>'userdetails', 'class'=>'user-name current-user'));
+                        echo html_writer::tag('span', $USER->firstname.'&nbsp;'.$USER->lastname);
+                        echo html_writer::start_tag('li', array('class'=>'prolog'));
+                        echo html_writer::link(new moodle_url('/user/profile.php', array('id'=>$USER->id)), get_string('myprofile')).' | ';
+                        echo html_writer::end_tag('li');
+                        echo html_writer::start_tag('li', array('class'=>'prolog'));
+                        echo html_writer::link(new moodle_url('/login/logout.php', array('sesskey'=>sesskey())), get_string('logout'));
+                        echo html_writer::end_tag('li');
+                        echo html_writer::end_tag('ul');
+                        } else {
+                            echo html_writer::start_tag('ul', array('class'=>"userbsar-links"));
+                            $loginlink = html_writer::link(new moodle_url('/login/'), get_string('loginhere', 'theme_spbspu'));
+                            echo html_writer::tag('li', get_string('welcome', 'theme_spbspu', $loginlink));
+                            $loginlink = html_writer::link(new moodle_url('/login/signup.php'), get_string('newaccount', 'theme_spbspu'));
+                            echo html_writer::tag('li', $loginlink);
+                            echo html_writer::end_tag('ul');;
+                        }
+                    ?>
+            </div>
+
             <div id="page-header-wrapper" class="wrapper clearfix">
                 <?php if ($hasheading) { ?>
                 <div id="headermenu">
-                    <?php if (isloggedin()) {
-                    echo html_writer::tag('div', $OUTPUT->user_picture($USER, array('size'=>24)), array('class'=>'userimg'));
-                    echo html_writer::start_tag('div', array('id'=>'userdetails'));
-                    echo html_writer::tag('h1', get_string('usergreeting', 'theme_spbspu', $USER->firstname));
-                    echo html_writer::start_tag('p', array('class'=>'prolog'));
-                    echo html_writer::link(new moodle_url('/user/profile.php', array('id'=>$USER->id)), get_string('myprofile')).' | ';
-                    echo html_writer::link(new moodle_url('/login/logout.php', array('sesskey'=>sesskey())), get_string('logout'));
-                    echo html_writer::end_tag('p');
-                    echo html_writer::end_tag('div');
-                    } else {
-                        echo html_writer::start_tag('div', array('id'=>'userdetails_loggedout'));
-                        $loginlink = html_writer::link(new moodle_url('/login/'), get_string('loginhere', 'theme_spbspu'));
-                        echo html_writer::tag('h1', get_string('welcome', 'theme_spbspu', $loginlink));
-                        echo html_writer::end_tag('div');;
-                    } ?>
-                    <div class="clearer"></div>
-
-                    <?php echo $OUTPUT->lang_menu();?>
                 </div>
-                <div id="logobox">
-                    <?php if ($haslogo) {
-                        echo html_writer::link(new moodle_url('/'), "<img src='".$PAGE->theme->settings->logo."' alt='logo' />");
-                    } else {
-                        echo html_writer::link(new moodle_url('/'), $PAGE->heading, array('class'=>'nologoimage'));
-                    } ?>
-                    <?php if (!$hidetagline) { ?>
-                        <h4><?php echo $tagline ?></h4>
-                    <?php } ?>
-                </div>
-                <div class="clearer"></div>
                 <?php if ($haslogo) { ?>
                 <h4 class="headermain inside">&nbsp;</h4>
                 <?php } else { ?>
                 <h4 class="headermain inside"><?php echo $PAGE->heading ?></h4>
                 <?php } ?>
             <?php } // End of if ($hasheading)?>
+            </div>
+        </div>
+    <?php } // if ($hasheading) ?>
+        <!-- END OF HEADER -->
+
+        <!-- START OF CONTENT -->
+        <div id="content">
+            <?php if ($hassidepre && $hassidepost) { ?>
+            <div id="sidebar">
+
+            <?php if ($hassidepre) { ?>
+            <div id="region-pre" class="block-region">
+                <div class="region-content">
+                    <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
+                </div>
+            </div>
+            <?php } ?>
+
+            <?php if ($hassidepost) { ?>
+            <div id="region-post" class="block-region">
+                <div class="region-content">
+                    <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                </div>
+            </div>
+            <?php } ?>
+            </div>
+            <?php } ?>
+
+            <div class="content-box">
                 <!-- DROP DOWN MENU -->
-                <div class="clearer"></div>
+                <?php if($hasnavbar) { ?>
                 <div id="dropdownmenu">
                     <?php if ($hascustommenu) { ?>
                     <div id="custommenu"><?php echo $custommenu; ?></div>
                     <?php } ?>
                     <div class="navbar">
-                        <div class="wrapper clearfix">
-                            <div class="breadcrumb"><?php if ($hasnavbar) echo $OUTPUT->navbar(); ?></div>
-                            <div class="navbutton"> <?php echo $PAGE->button; ?></div>
-                        </div>
+                        <div class="breadcrumb"><?php if ($hasnavbar) echo $OUTPUT->navbar(); ?></div>
+                        <div class="navbutton"> <?php echo $PAGE->button; ?></div>
                     </div>
                 </div>
+                <?php } // End of if ($hasnavbar)?>
                 <!-- END DROP DOWN MENU -->
-            </div>
-        </div>
-    <?php } // if ($hasheading || $hasnavbar) ?>
-        <!-- END OF HEADER -->
-        <!-- START OF CONTENT -->
-        <div id="page-content">
-            <div id="region-main-box">
-                <div id="region-post-box">
-                    <div id="content-wrap">
-                        <div id="content">
-                            <div id="content-box">
-                                <div id="region-main">
-                                    <div class="region-content">
-                                        <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div id="region-main">
+                    <div class="region-content">
+                        <?php echo core_renderer::MAIN_CONTENT_TOKEN ?>
                     </div>
-
-                    <?php if ($hassidepre && $hassidepost) { ?>
-                    <div id="sidebar">
-
-                    <?php if ($hassidepre) { ?>
-                    <div id="region-pre" class="block-region">
-                        <div class="region-content">
-                            <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
-                        </div>
-                    </div>
-                    <?php } ?>
-
-                    <?php if ($hassidepost) { ?>
-                    <div id="region-post" class="block-region">
-                        <div class="region-content">
-                            <?php echo $OUTPUT->blocks_for_region('side-post') ?>
-                        </div>
-                    </div>
-                    <?php } ?>
-                    </div>
-                    <?php } ?>
                 </div>
             </div>
+
         </div>
         <!-- END OF CONTENT -->
         <div class="clearfix"></div>
-    <!-- END OF #Page -->
-    </div>
+
     <!-- START OF FOOTER -->
     <?php if ($hasfooter) { ?>
     <div id="footer">
-	<div id="footer-wrapper">
-            <?php if ($hasfootnote) { ?>
-            <div id="footnote"><?php echo $PAGE->theme->settings->footnote; ?></div>
-            <?php } ?>
-            <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
-            <?php
-            echo $OUTPUT->login_info();
-            echo $OUTPUT->home_link();
-            echo $OUTPUT->standard_footer_html();
-            ?>
-        </div>
+        <div id="footer-wrapper">
+            <div id="footer-first">
+                <p>© 2011 СПбГПУ</p>
+                <?php if ($hasfootnote) { ?>
+                    <div id="footnote"><?php echo $PAGE->theme->settings->footnote; ?></div>
+                <?php } ?>
+            </div>
+            <div id="footer-second">
+                <?php
+                echo $OUTPUT->login_info();
+                echo $OUTPUT->home_link();
+                echo $OUTPUT->standard_footer_html();?>
+                <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
+            </div>
+    </div>
     </div>
     <?php } ?>
+    </div>
 <?php echo $OUTPUT->standard_end_of_body_html() ?>
 </body>
 </html>
