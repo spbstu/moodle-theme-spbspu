d364fed900d67e7e28f91b989acc397b0ba88220
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-04-20 17:28:56 +0400

    Изменение оформления шапки

--- layout/general.php
+++ layout/general.php
@@ -49,55 +49,64 @@ echo $OUTPUT->doctype() ?>
     <div id="page" class="<?php if($hasnavbar){?>with-navbar<?php } else {?>without-navbar<?php } ?>">
         <?php if ($hasheading) { ?>
         <div id="header">
-            <div class="header-wrapper">
-            <div id="logo">
-                <?php if ($haslogo) {
-                        echo html_writer::link(new moodle_url('/'), "<img src='".$PAGE->theme->settings->logo."' alt='logo' class='logo' />");
-                    } ?>
-                <h5 class="subtitle"><?php echo $tagline ?></h5>
-                <h2 class="site-name"><?php echo html_writer::link(new moodle_url('/'), $SITE->fullname); ?></h2>
-                 
-            </div>
-            <div id="userbar">
-                    <?php if (isloggedin()) {
-                        echo html_writer::start_tag('ul', array('class'=>"userbsar-links"));
-                        echo html_writer::tag('span', $OUTPUT->user_picture($USER, array('size'=>18)), array('class'=>'userimg'));
-                        echo html_writer::start_tag('li', array('id'=>'userdetails', 'class'=>'user-name current-user'));
-                        echo html_writer::link(new moodle_url('/user/profile.php', array('id'=>$USER->id)), $USER->firstname.'&nbsp;'.$USER->lastname);
-                        echo html_writer::end_tag('li');
-                        echo html_writer::start_tag('li', array('class'=>'prolog'));
-                        echo html_writer::link(new moodle_url('/login/logout.php', array('sesskey'=>sesskey())), get_string('logout'));
-                        echo html_writer::end_tag('li');
-                        echo html_writer::end_tag('ul');
-                        } else {
+            
+            <div class="logobar">
+                <div class="header-wrapper">
+                    <div id="logo">
+                        <?php if ($haslogo) {
+                                echo html_writer::link(new moodle_url('/'), "<img src='".$PAGE->theme->settings->logo."' alt='logo' class='logo' />");
+                            } ?>
+                        <h5 class="subtitle"><?php echo $tagline ?></h5>
+                        <h2 class="site-name"><?php echo html_writer::link(new moodle_url('/'), $SITE->fullname); ?></h2>
+                         
+                    </div>
+                    <div id="userbar">
+                        <?php if (isloggedin()) {
                             echo html_writer::start_tag('ul', array('class'=>"userbsar-links"));
-                            $loginlink = html_writer::link(new moodle_url('/login/'), get_string('loginhere', 'theme_spbspu'));
-                            echo html_writer::tag('li', get_string('welcome', 'theme_spbspu', $loginlink));
-                            $signuplink = html_writer::link(new moodle_url('/login/signup.php'), get_string('newaccount', 'theme_spbspu'));
-                            echo html_writer::tag('li', $signuplink);
-                            echo html_writer::end_tag('ul');;
-                        }
-                    ?>
+                            echo html_writer::tag('span', $OUTPUT->user_picture($USER, array('size'=>18)), array('class'=>'userimg'));
+                            echo html_writer::start_tag('li', array('id'=>'userdetails', 'class'=>'user-name current-user'));
+                            echo html_writer::link(new moodle_url('/user/profile.php', array('id'=>$USER->id)), $USER->firstname.'&nbsp;'.$USER->lastname);
+                            echo html_writer::end_tag('li');
+                            echo html_writer::start_tag('li', array('class'=>'prolog'));
+                            echo html_writer::link(new moodle_url('/login/logout.php', array('sesskey'=>sesskey())), get_string('logout'));
+                            echo html_writer::end_tag('li');
+                            echo html_writer::end_tag('ul');
+                            } else {
+                                echo html_writer::start_tag('ul', array('class'=>"userbsar-links"));
+                                $loginlink = html_writer::link(new moodle_url('/login/'), get_string('loginhere', 'theme_spbspu'));
+                                echo html_writer::tag('li', get_string('welcome', 'theme_spbspu', $loginlink));
+                                $signuplink = html_writer::link(new moodle_url('/login/signup.php'), get_string('newaccount', 'theme_spbspu'));
+                                echo html_writer::tag('li', $signuplink);
+                                echo html_writer::end_tag('ul');;
+                            }
+                        ?>
+                    </div>
                 </div>
-               <?php if($hasnavbar) { ?>
-                    <div class="navbutton"> <?php echo $PAGE->button; ?></div>
-                <?php }?>
-             <div id="page-header-wrapper" class="wrapper clearfix">
-                <?php if ($hasheading) { ?>
-                <div id="headermenu">
-                </div>
-                <?php if ($haslogo) { ?>
-                <h4 class="headermain inside">&nbsp;</h4>
-                <?php } else { ?>
-                <h4 class="headermain inside"><?php echo $PAGE->heading ?></h4>
-                <?php } ?>
-            <?php } // End of if ($hasheading)?>
-
             </div>
-            <?php if($hasnavbar) { ?>
-            <div class="breadcrumb"><?php if ($hasnavbar) { echo $OUTPUT->navbar();} ?></div>
-            <?php } ?>
-
+            <div class="navbar">
+                <div class="header-wrapper">
+                    <?php if($hasnavbar) { ?>
+                        <div class="navbutton"> <?php echo $PAGE->button; ?></div>
+                    <?php }?>
+                    <?php
+                    /* if ($hasheading) { ?>
+                     <div id="page-header-wrapper" class="wrapper clearfix">
+                        
+                        <div id="headermenu">
+                        </div>
+                        <?php if ($haslogo) { ?>
+                        <h4 class="headermain inside">&nbsp;</h4>
+                        <?php } else { ?>
+                        <h4 class="headermain inside"><?php echo $PAGE->heading ?></h4>
+                        <?php } ?>
+                    </div>
+                    <?php } // End of if ($hasheading)
+                    */
+                    ?>
+                    <?php if($hasnavbar) { ?>
+                    <div class="breadcrumb"><?php if ($hasnavbar) { echo $OUTPUT->navbar();} ?></div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     <?php } // if ($hasheading) ?>
