847517753dc07925ecd59b179715fb940879d3f6
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-04-20 19:43:58 +0400

    Прячем сайдбар, если его не нужно показывать

--- javascript/spbstu.js
+++ javascript/spbstu.js
@@ -12,8 +12,33 @@ $(function(){
 
 
 YUI().use('node', 'event', function(Y) {
-    var admin_block = Y.one('.block_adminblock');
+    var admin_block = Y.one('.block_adminblock'),
+        sidebar = Y.one('#sidebar'),
+        sidebarHeight;
+    
     if (admin_block) {
         admin_block.wrap('<div class="block_adminblock-container">').wrap('<div class="block_adminblock-wrapper">');
     }
-    });
+
+    if (sidebar) {
+        sidebarHeight = sidebar.get('offsetHeight');
+        Y.one(window).on('scroll', function(e) {
+            controlSidebar();
+        });
+    }
+
+    function controlSidebar() {
+        var body = Y.one('body');
+        if (sidebar.get('docScrollY') > sidebarHeight) {
+            if (sidebar.get('offsetHeight') > sidebarHeight) {
+                sidebarHeight = sidebar.get('offsetHeight');
+                return;
+            }
+            sidebar.addClass('sidebar-hidden');
+            body.addClass('content-only');
+        } else {
+            sidebar.removeClass('sidebar-hidden');
+            body.removeClass('content-only');
+        }
+    }
+});
