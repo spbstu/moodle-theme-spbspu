d14afa3607628f920ff7b94eac2eb3ce844b4047
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-09-11 13:07:07 +0400

    typechange of lang file

--- lang/en/theme_spbspu.php
+++ lang/en/theme_spbspu.php
@@ -1,1 +1,46 @@
-../ru/theme_spbspu.php
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_spbspu', language 'ru'
+ *
+ * @package   theme_standard
+ * @copyright 2010 Caroline Kennedy of Synergy Learning
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['configtitle'] = 'СПбГПУ – настройки темы';
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Any CSS you enter here will be added to every page allowing your to easily customise this theme.';
+$string['defaulttagline'] = 'Система дистанционного обучения';
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'The content from this textarea will be displayed in the footer of every page. E.g: your copyright info.';
+$string['hide_tagline'] = 'Hide tagline';
+$string['hide_taglinedesc'] = 'Check this box to hide the tagline under the logo.';
+$string['loginhere'] = 'Войти';
+$string['newaccount'] = 'Зарегистрироваться';
+$string['logo'] = 'Логотип';
+$string['logodesc'] = 'Enter the URL to an image to use as the logo for this site. E.g: http://www.yoursite.com/path/to/logo.png. The logo should be max 230px wide.';
+$string['pluginname'] = 'spbspu';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['region-top'] = 'Top';
+$string['tagline'] = 'Tagline';
+$string['taglinedesc'] = 'The content from this textarea will be displayed under the Site logo on the Homepage';
+$string['usergreeting'] = 'Привет, {$a}!';
+$string['welcome'] = '{$a}';
